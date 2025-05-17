2c37afae44b392ce3ca8bdac10cbad962ea1dddb
Jaine Brito <jaine.silva@paipe.co>
2025-05-17 10:42:57 -0400

    Adicionando Aula 1 do módulo Python

---
+++ aula1.py
@@ -0,0 +1,65 @@
+# print("Hello World")
+# print(1+1)
+
+ #int
+# idade = int(input())
+# idade: int
+
+# #float
+# peso = float(input())
+# peso: float
+
+# #bool
+# cansado: bool
+# cansado = False
+
+# #String
+# endereco: str
+# endereco = 'Rua A, 100'
+
+# #Lista
+# compras: list
+# compras = ["arroz", "carne", "verduras"
+# meu_estoque: list = ["arroz", 2,5.2, "feijão", 2,3.90]
+
+# #Conjuntos
+# compras:set
+# compras = ["arroz", "carne", "verduras"]
+# item_estoque: set = {"arroz", 2,5.20}
+
+# compras: dict
+# compras = {"arroz":"2kg", "carne":"3kg"}
+# precos: dict 
+
+#Inteiro
+idade = int(26)
+print(type(idade))
+
+#Float
+salario = float(1000.0)
+print(type(salario))
+
+#Bool
+cansado = True
+print(type(cansado))
+
+#String
+nome = 'Jay'
+print(type(nome))
+
+#Lista
+compras = ["arroz", "Feijão", "Macarrão"]
+print(type(compras))
+
+#Conjunto
+compras = {"arroz", "carne", "verduras"}
+print(type(compras))
+
+#Tuplas
+comprado = ("Arroz", 2, 5.50 ,"Feijão",2, 5.50,  "Macarrão", 2, 5.50)
+print(type(comprado))
+
+
+#Dicionário
+compras = {"arroz":"2kg", "carne":"3kg"}
+print(type(compras))
